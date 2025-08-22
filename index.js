8c6e8381ff42a67752b893cf356e977e2d95f5ac
ShivrajGoswamiChess48 <shivrajgoswamichess48@gmail.com>
2025-08-22 16:35:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const xValues = ["September 2025", "October 2025", "November 2025", "December 2025", "January 2026"];
-const yValues = [800, 800, 800, 800, 800];
-const barColors = ["blue", "blue","blue","blue","blue"];
+const xValues = ["September 2025", "October 2025", "November 2025", "December 2025"];
+const yValues = [800, 800, 800, 800];
+const barColors = ["blue", "blue","blue","blue"];
 
 new Chart("myChart", {
   type: "line",
